668a9d50f1f92dd2c16c41826d063410b2a5a336
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-21 18:49:04 +0800

    model

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
         (STATUS_DELETE, '删除'),
     )
     name = models.CharField(max_length=50, verbose_name='姓名')
-    status = models.PositiveIntegerField(default=STATUS_ITEM, choices=STATUS_ITEM, verbose_name='状态')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEM, verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
     owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
@@ -27,7 +27,7 @@ class Tag(models.Model):
         (STATUS_DELETE, '删除'),
     )
     name = models.CharField(max_length=50, verbose_name='标签名')
-    status = models.PositiveIntegerField(default=STATUS_ITEM, choices=STATUS_ITEM, verbose_name='状态')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEM, verbose_name='状态')
     owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
@@ -38,14 +38,16 @@ class Tag(models.Model):
 class Post(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
+    STATUS_DRAFT = 2
     STATUS_ITEM = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
+        (STATUS_DRAFT, '草稿'),
     )
     title = models.CharField(max_length=255, verbose_name='标签')
     desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(help_text='正文需要MarkDown格式', verbose_name='正文')
-    status = models.PositiveIntegerField(default=STATUS_ITEM, choices=STATUS_ITEM, verbose_name='状态')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEM, verbose_name='状态')
     category = models.ForeignKey(Category, verbose_name='分类')
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     owner = models.ForeignKey(User, verbose_name='作者')
