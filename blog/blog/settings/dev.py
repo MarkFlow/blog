c5e74cc2909e0faf5dec79a81335b027164b7a22
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-21 19:05:31 +0800

    sql

--- blog/blog/settings/dev.py
+++ blog/blog/settings/dev.py
@@ -7,7 +7,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWORD': 'sjdueongh1',
+        'HOST': '192.168.255.133',
+        'PORT': '3306',
     }
 }
