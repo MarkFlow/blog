7192ea3c51e39f06d32addaa13394e221e9903ea
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-29 11:34:05 +0800

    admin

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -18,6 +18,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -33,6 +36,9 @@ class Tag(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -56,4 +62,7 @@ class Post(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']   # id字段降序排列
+
+    def __str__(self):
+        return self.name
 
