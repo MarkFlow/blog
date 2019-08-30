2da86268389a3a8ef9d54feed7a6d70224f074c4
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-30 12:01:33 +0800

    author base class

--- blog/comment/models.py
+++ blog/comment/models.py
@@ -20,7 +20,7 @@ class Comment(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
-        verbose_name = verbose_name_plural = '友链'
+        verbose_name = verbose_name_plural = '评论'
 
  #   def __str__(self):
  #       return self.name
